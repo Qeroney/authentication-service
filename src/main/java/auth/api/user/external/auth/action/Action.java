10da93ae303eadab4ddf5d1955c9a27b6d5298fe
v.vetrov <v.vetrov@thewhite.ru>
2025-02-12 22:19:50 +1000

    Удалил лишнее

--- src/main/java/auth/api/user/external/auth/action/Action.java
+++
@@ -1,7 +0,0 @@
-package auth.api.user.external.auth.action;
-
-import lombok.NonNull;
-
-public interface Action<ArgumentT, ReturnedT> {
-    ReturnedT execute(@NonNull ArgumentT var1);
-}
--- src/main/java/auth/api/user/external/auth/action/VoidAction.java
+++
@@ -1,9 +0,0 @@
-package auth.api.user.external.auth.action;
-
-import lombok.NonNull;
-
-public interface VoidAction<ArgumentT> {
-    void execute(@NonNull ArgumentT var1);
-}
-
-
